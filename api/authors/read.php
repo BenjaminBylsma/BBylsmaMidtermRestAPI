7e2fa4d1b38fb09183ef584bc892232aff60d9f6
Benjamin Bylsma <benjaminbylsma5@gmail.com>
2024-03-19 13:19:50 -0500

    pulled authors pushing to a data key inside the authors array

--- api/authors/read.php
+++ api/authors/read.php
@@ -17,7 +17,6 @@ $num = $result->rowCount();
 if($num > 0){
     //post array
     $authors_arr = array();
-    $authors_arr['data'] = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)){
         extract($row);
@@ -26,7 +25,7 @@ if($num > 0){
             'id' => $id,
             'author' => $author
         );
-        array_push($authors_arr['data'], $authors_item);
+        array_push($authors_arr, $authors_item);
     } 
 
     //turn to JSON for output
